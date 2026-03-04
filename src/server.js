95a68064fbfb99c55fd14e22e810ef83e415a712
hema-1604 <hema@gmail.com>
2026-03-04 20:01:24 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -16,27 +16,8 @@ const app = express();
 
 // 🔹 Helmet for security headers
 app.use(helmet());
-
-// 🔹 CORS - restrict to your frontend
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//   credentials: true,
-// }));
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5173",
-//     "https://chat-frontend-an4r.vercel.app"
-//   ],
-//   credentials: true
-// }))
-import cors from "cors";
-
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://chat-frontend-an4r.vercel.app",
-    "https://chat-frontend-an4r-git-main-hemalathaponnada16s-projects.vercel.app"
-  ],
+  origin: true,
   credentials: true
 }));
 
